cf9b91ddff0e6d848f3d1a164fbb0ef97ed8577c
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2022-07-15 22:21:14 +0300

    Add files via upload

---
+++ variable.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+
+  String personName ="meme";
+
+  personName="salam";
+
+  int personAge =21;
+
+  double gpa =3.4;
+
+  bool isHeMale = true;
+
+  print("hello ,my name is ${personName}");
+
+  print("i'm ${personAge} years old");
+
+  print("i like my nsme ${personName}");
+
+ print("but, i don't like being ${personAge} years old");
+
+ print(personName);
+
+ print(personAge);
+
+ print(gpa);
+
+ print(isHeMale);
+ 
+}
